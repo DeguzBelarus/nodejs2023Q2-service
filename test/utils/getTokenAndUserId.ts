713a6a75fb875bbf1adc9bf11fe3603ab48122f7
Anton Dektyarev <90214660+DeguzBelarus@users.noreply.github.com>
2023-07-26 13:39:33 +0300

    Initial commit

---
+++ test/utils/getTokenAndUserId.ts
@@ -0,0 +1,34 @@
+import { authRoutes } from '../endpoints';
+
+const createUserDto = {
+  login: 'TEST_AUTH_LOGIN',
+  password: 'Tu6!@#%&',
+};
+
+const getTokenAndUserId = async (request) => {
+  // create user
+  const {
+    body: { id: mockUserId },
+  } = await request
+    .post(authRoutes.signup)
+    .set('Accept', 'application/json')
+    .send(createUserDto);
+
+  // get token
+  const {
+    body: { accessToken },
+  } = await request
+    .post(authRoutes.login)
+    .set('Accept', 'application/json')
+    .send(createUserDto);
+
+  if (mockUserId === undefined || accessToken === undefined) {
+    throw new Error('Authorization is not implemented');
+  }
+
+  const token = `Bearer ${accessToken}`;
+
+  return { token, mockUserId };
+};
+
+export default getTokenAndUserId;
