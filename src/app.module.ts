0a25d888e3b885951d04e8b5419a3d599f6d653d
DeguzBelarus <deguz@mail.ru>
2023-07-29 18:30:33 +0300

    refactor: implement env variables using nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
@@ -8,6 +9,7 @@ import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     ArtistModule,
     AlbumModule,
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { config } from 'dotenv';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-config();
 const PORT = parseInt(process.env.PORT, 10) || 4000;
 
 async function bootstrap() {
