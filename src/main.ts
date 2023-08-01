22065b16b334c02b47523837df6d345866221b9e
DeguzBelarus <deguz@mail.ru>
2023-08-01 22:03:03 +0300

    refactor: use IIFE for bootstrap function and add console message on server start

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 const PORT = parseInt(process.env.PORT, 10) || 4000;
 
-async function bootstrap() {
+(async function () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
-}
-bootstrap();
+  await app.listen(PORT, () =>
+    console.log(`Server has been started on port ${PORT}...`),
+  );
+})();
