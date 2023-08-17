c588e8457bb407fbb357cc9e21830fe79fa49571
DeguzBelarus <deguz@mail.ru>
2023-08-17 23:58:37 +0300

    fix: fix getFormattedDate util method

--- src/logger/utils.ts
+++ src/logger/utils.ts
@@ -2,9 +2,9 @@ interface IFormattedDateData {
   day: number | string;
   month: number | string;
   year: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
+  hours: number | string;
+  minutes: number | string;
+  seconds: number | string;
 }
 
 export const getFormattedDate = (): IFormattedDateData => {
@@ -14,9 +14,12 @@ export const getFormattedDate = (): IFormattedDateData => {
   let month: number | string = dateData.getMonth();
   month = month < 10 ? `0${month}` : month;
   const year = dateData.getFullYear();
-  const hours = dateData.getHours();
-  const minutes = dateData.getMinutes();
-  const seconds = dateData.getSeconds();
+  let hours: number | string = dateData.getHours();
+  hours = hours < 10 ? `0${hours}` : hours;
+  let minutes: number | string = dateData.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  let seconds: number | string = dateData.getSeconds();
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
   return {
     day,
     month,
